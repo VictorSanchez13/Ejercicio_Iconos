7902bb3a365cccc2905ff61b614fd935e446a4bb
Victor <victor.sr.2a@gmail.com>
2020-11-12 18:18:36 +0100

    iconos

---
+++ Icono_Estrella/app/src/main/java/mi/app/icono_estrella/MainActivity.java
@@ -0,0 +1,14 @@
+package mi.app.icono_estrella;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
